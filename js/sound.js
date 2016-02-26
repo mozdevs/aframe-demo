a88c57f216361711a489105fea022e68e094a1dd
Belén Albeza <belen.albeza@gmail.com>
2016-02-26 15:29:59 +0100

    use chords instead of single notes

--- js/sound.js
+++ js/sound.js
@@ -9,13 +9,31 @@ const NOTES = {
     'D4': 293.66,
     'E4': 329.63,
     'F4': 349.23,
+    'F4#': 370,
     'G4': 392,
+    'G4#': 415.30,
     'A4': 440,
     'B4': 493.88,
-    'C5': 523.25
+    'C5': 523.25,
+    'C5#': 554.4,
+    'D5': 587.3,
+    'E5b': 622.3,
+    'E5': 659.3,
+    'F5#': 740.0 
+};
+
+const CHORDS = {
+    'C': ['C4', 'E4', 'G4'],
+    'D': ['D4', 'F4#', 'A4'],
+    'E': ['E4', 'G4#', 'B4'],
+    'F': ['F4', 'A4', 'C5'],
+    'G': ['G4', 'B4', 'D5'],
+    'A': ['A4', 'C5#', 'E5'],
+    'B': ['B4', 'E5b', 'F5#']
 };
 
 const NOTE_NAMES = Object.keys(NOTES);
+const CHORD_NAMES = Object.keys(CHORDS);
 
 const NOTE_DUR = 2; // seconds
 
@@ -62,7 +80,11 @@ window.onload = function () {
     let loop = function () {
         let timestamp = ctx.currentTime;
         if (timestamp - lastNoteTime > NOTE_DUR) {
-            let note = NOTE_NAMES[Math.floor(Math.random() * NOTE_NAMES.length)];
+            let chordName = CHORD_NAMES[Math.floor(
+                Math.random() * CHORD_NAMES.length)];
+                console.log(chordName, CHORDS[chordName]);
+            let note = CHORDS[chordName][0];
+
             let speaker = document.querySelector(`#${note.toLowerCase()}`);
             speaker.emit('sound');
 
@@ -75,7 +97,11 @@ window.onload = function () {
                 z: -Math.sin(radians(rotY) + Math.PI/2)
             };
 
-            instrument.play(NOTES[note], NOTE_DUR, position, orientation);
+            console.log('playing', CHORDS[chordName]);
+            CHORDS[chordName].forEach(function (note) {
+                instrument.play(NOTES[note], NOTE_DUR, position, orientation);
+            });
+
             lastNoteTime = timestamp;
         }
         requestAnimationFrame(loop);
